04c2185404ecf96be758e5553c3249500bc568e5
¨iuliia¨ <yuliya.glazeva@gmail.com>
2018-02-17 17:06:23 +0100

    created method 'output_contacts'

--- main.rb
+++ main.rb
@@ -4,4 +4,11 @@ def read_contacts
   contacts_json = File.read("contacts.json")
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
-puts read_contacts
+
+contact = [{"Arno Fleming" =>  "+31 6 414 14141"},
+           {"Mike Farrell" =>  "+1 123 123 1234"}]
+
+def output_contacts(contact)
+  contact.map{|i| i}
+end
+p output_contacts(contact)
