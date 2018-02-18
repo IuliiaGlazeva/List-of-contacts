643eda5d7f43ae858306e624f3a125b7efa14ce2
¨iuliia¨ <yuliya.glazeva@gmail.com>
2018-02-18 18:27:30 +0100

    added method 'reduce_contact'

--- main.rb
+++ main.rb
@@ -9,16 +9,30 @@ end
 
 contact = [
   {
-    :name => "Thomas Jefferson",
-    :phone => "+1 206 310 1369",
-    :email => "tjeff@us.gov"
+    "name": "Thomas Jefferson",
+    "phone": "+1 206 310 1369",
+    "email": "tjeff@us.gov"
   },
   {
-    :name => "Charles Darwin",
-    :phone => "+2 360 155 8822",
-    :email => "darles@evolve.org"
+    "name": "Charles Darwin",
+    "phone": "+44 20 7123 4567",
+    "email": "darles@evolve.org"
+  },
+  {
+    "name": "Nikola Tesla",
+    "phone": "+385 43 987 3355",
+    "email": "nik@inductlabs.com"
+  },
+  {
+    "name": "Genghis Khan",
+    "phone": "+976 2 194 2222",
+    "email": "contact@empire.com"
+  },
+  {
+    "name": "Malcom X",
+    "phone": "+1 310 155 8822",
+    "email": "x@theroost.org"
   }
-
 ]
 
 def output_contacts(contact)
@@ -32,7 +46,14 @@ def select_contact1(contact)
 end
 p select_contact1(contact)
 
+
 def select_contact2(contact)
   contact.select {|i| i[:email].end_with?("org")}
 end
 p select_contact2(contact)
+
+def reduce_contact1(contact)
+  select_contact1(contact).reduce(0){ |sum, i|
+   sum += 1}
+end
+puts reduce_contact1(contact)
